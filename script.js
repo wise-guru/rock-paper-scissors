a73fbe68b05a21c90e2bbab2abb9b7fae2b2326a
wise-guru <wise.guru123@gmail.com>
2022-05-27 15:15:13 -0400

    Add buttons and scoreboard to html page

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 let computerScore = 0;
 let playerScore = 0;
 let roundResult;
+let playerSelection;
+
+function playerClick() {
+    const rock = document.querySelector('#rock');
+    rock.addEventListener('click', function() {
+        playRound("rock")
+    });
+
+    const paper = document.querySelector('#paper');
+    paper.addEventListener('click', function() {
+        playRound("paper")
+    });
+
+    const scissors = document.querySelector('#scissors');
+    scissors.addEventListener('click', function() {
+        playRound("scissors");
+    }); 
+}
+
+playerClick();
+
+const scoreBox = document.querySelector('#scoreBox');
+    
+    const roundResultText = document.createElement('h2');
+    roundResultText.classList.add('roundResultText');
+    roundResultText.textContent = "Round result: " + roundResult;   
+    scoreBox.appendChild(roundResultText);
+
+    const score = document.createElement('h1')
+    score.classList.add('score');
+    score.textContent = ('Score');
+    scoreBox.appendChild(score);
+
+    const compScore = document.createElement('p');
+    compScore.classList.add('compScore');
+    compScore.textContent = 'Computer score: ' + computerScore;
+    scoreBox.appendChild(compScore);
+
+    const userScore = document.createElement('p');
+    userScore.classList.add('userScore');
+    userScore.textContent = 'Player score: ' + playerScore;
+    scoreBox.appendChild(userScore);
+
 
 //For the computer to randomly return rock, paper, or scissors//
 function computerPlay() {
@@ -14,66 +57,40 @@ function computerPlay() {
     }
 }
 
+
 //Single round of Rock Paper Scissors//
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection) {
+let computerSelection = computerPlay();
+
     //The round being a tie//
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    if (playerSelection === computerSelection) {
         return roundResult = ("It's a tie!");
     }   
     
     //Player wins the round//
-    else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        playerScore++
-        return roundResult = ("You win! Rock beats Scissors.");
-    }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        playerScore++
-        return roundResult = ("You win! Paper beats Rock.");
-    }
-    else if (playerSelection.toLowerCase()=== "scissors" && computerSelection.toLowerCase() === "paper") {
-        playerScore++
-        return roundResult = ("You win! Scissors beat Paper.");
-    }
+    else if ((playerSelection === "rock" && computerSelection  === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+            playerScore+= 1;
+            roundResult = ("You win! " + playerSelection + "beats " + computerSelection + " .");
+
+            if (playerScore === 5) {
+                result = ("You won! Way to show our robot overlords.");
+            }
+        }  
     
     //Computer wins the round//
-    else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
-        computerScore++
-        return roundResult = ("You lose! Rock beats Scissors.");
-    }
-    else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
-        computerScore++
-        return roundResult = ("You lose! Paper beats Rock.");
-    }
-    else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
-        computerScore++
-        return roundResult = ("You lose! Scissors beat Paper.");
+    else if ((computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")) {
+        computerScore += 1;
+        roundResult = ("You lose! " + playerSelection + "beats " + computerSelection + " .");
+
+        if (computerScore === 5) {
+            result = ("You lose! The robot overlords have won.");
+        }
     }
 }
 
-/*Loop the game 5 times
+playRound(playerSelection);
 
-function game() {
-    
-
-    for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Rock, paper, or scissors?");
-    const computerSelection = computerPlay();
-    console.log(playRound (playerSelection, computerSelection));
-    
-
-        if ( i === 4 && playerScore > computerScore ) {
-            console.log("You won! Way to show our robot overlords.");
-        }
-        else if (i === 4 && computerScore > playerScore) {
-            console.log("You lose! The robot overlords have won.");
-        }
-        else if (i=== 4 && playerScore === computerScore) {
-            console.log("")
-        }
-    }
-
-}  
-
-game();
-
-*/
