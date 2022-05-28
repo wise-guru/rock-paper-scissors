4fcfe80fa34a598870cfa7bee9313cfb81ccc54f
wise-guru <wise.guru123@gmail.com>
2022-05-27 23:06:38 -0400

    Disable buttons and refresh button added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let playerScore = 0;
 let roundResult;
 let playerSelection;
 
+
 function playerClick() {
     const rock = document.querySelector('#rock');
     rock.addEventListener('click', function() {
@@ -26,7 +27,7 @@ const scoreBox = document.querySelector('#scoreBox');
     
     const roundResultText = document.createElement('h2');
     roundResultText.classList.add('roundResultText');
-    roundResultText.textContent = "Round result: " + roundResult;   
+    roundResultText.textContent = "Pick rock, paper, or scissors.";   
     scoreBox.appendChild(roundResultText);
 
     const score = document.createElement('h1')
@@ -34,15 +35,17 @@ const scoreBox = document.querySelector('#scoreBox');
     score.textContent = ('Score');
     scoreBox.appendChild(score);
 
-    const compScore = document.createElement('p');
-    compScore.classList.add('compScore');
-    compScore.textContent = 'Computer score: ' + computerScore;
-    scoreBox.appendChild(compScore);
+    const compScoreText = document.createElement('p');
+    compScoreText.classList.add('compScoreText');
+    compScoreText.textContent = `Computer score: ${computerScore}`;
+    scoreBox.appendChild(compScoreText);
 
-    const userScore = document.createElement('p');
-    userScore.classList.add('userScore');
-    userScore.textContent = 'Player score: ' + playerScore;
-    scoreBox.appendChild(userScore);
+
+
+    const userScoreText = document.createElement('p');
+    userScoreText.classList.add('userScoreText');
+    userScoreText.textContent = `Player score: ${playerScore}`;
+    scoreBox.appendChild(userScoreText);
 
 
 //For the computer to randomly return rock, paper, or scissors//
@@ -64,7 +67,7 @@ let computerSelection = computerPlay();
 
     //The round being a tie//
     if (playerSelection === computerSelection) {
-        return roundResult = ("It's a tie!");
+        return roundResultText.textContent = ("It's a tie!");
     }   
     
     //Player wins the round//
@@ -72,10 +75,13 @@ let computerSelection = computerPlay();
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
             playerScore+= 1;
-            roundResult = ("You win! " + playerSelection + "beats " + computerSelection + " .");
-
+            roundResultText.textContent = ("You win! " + playerSelection + " beats " + computerSelection + ".");
+            userScoreText.textContent = `Player score: ${playerScore}`;
+            
             if (playerScore === 5) {
-                result = ("You won! Way to show our robot overlords.");
+                roundResultText.textContent = ("You won! Way to show our robot overlords. Reload the page to play again.");
+                disableButtons();
+                refresh();
             }
         }  
     
@@ -85,12 +91,34 @@ let computerSelection = computerPlay();
     (computerSelection === "scissors" && playerSelection === "paper")) {
         computerScore += 1;
         roundResult = ("You lose! " + playerSelection + "beats " + computerSelection + " .");
+        compScoreText.textContent = `Computer score: ${computerScore}`;
 
         if (computerScore === 5) {
-            result = ("You lose! The robot overlords have won.");
+            result = ("You lose! The robot overlords have won. Reload the page to play again.");
+            disableButtons();
+            refresh();
         }
     }
 }
 
 playRound(playerSelection);
 
+function disableButtons() {
+    document.querySelector('#rock').disabled = true;
+    document.querySelector('#paper').disabled = true;
+    document.querySelector('#scissors').disabled = true;
+
+}
+
+function refresh() {
+    const refreshDiv = document.querySelector('#refresh');
+
+    const refreshBtn = document.createElement('button')
+    refreshBtn.addEventListener ('click', function() {
+        window.parent.location = window.parent.location.href;
+    })
+    refreshBtn.classList.add('refreshBtn');
+    refreshBtn.textContent = "Play again";
+    refreshDiv.appendChild(refreshBtn);
+    
+}
